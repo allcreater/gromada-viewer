9971a44a95bd2aa0b74d2fc85516e007c0c748ec
Anton Semenov <allcreater@gmail.com>
2025-04-02 14:58:09 +0300

    maps_selector: map name was cached (because std::filesystem::relative is costful operation)

--- src/view_models/map_selector.cpp
+++ src/view_models/map_selector.cpp
@@ -19,30 +19,36 @@ public:
 		ImGui::PushStyleVar(ImGuiStyleVar_WindowMinSize, ImVec2(300, 500));
 		ImGui::Begin("Maps");
 
-		auto pathToCStr = [this, currentStr = std::u8string{}](const auto& path) mutable {
-			currentStr = std::filesystem::relative(path, m_mapsBaseDirectory).u8string();
-			return reinterpret_cast<const char*>(currentStr.c_str());
-		};
+		auto pathToCStr = [this, currentStr = std::u8string{}](
+							  const MapEntry& mapEntry) mutable { return reinterpret_cast<const char*>(mapEntry.name.c_str()); };
 
 		if (ListBox("Maps", &m_selectedMap, std::span{m_maps}, std::move(pathToCStr))) {
 			const auto& selectedMap = m_maps[m_selectedMap];
-			if (selectedMap != m_model.map().filename()) {
-				m_model.loadMap(selectedMap);
+			if (selectedMap.path != m_model.map().filename()) {
+				m_model.loadMap(selectedMap.path);
 			}
 		}
 		ImGui::End();
 		ImGui::PopStyleVar();
 	}
 
-	static std::vector<std::filesystem::path> getMaps(const Model& model, const std::filesystem::path& mapsDirectory) {
-		return std::filesystem::recursive_directory_iterator{mapsDirectory} | std::views::transform([](const auto& entry) { return entry.path(); }) |
-			   std::views::filter([](const auto& path) { return path.extension() == ".map"; }) | std::ranges::to<std::vector>();
-	}
-
 private:
 	Model& m_model;
 	int m_selectedMap = 0;
 
+	struct MapEntry {
+		std::u8string name;
+		std::filesystem::path path;
+	};
+
+	static std::vector<MapEntry> getMaps(const Model& model, const std::filesystem::path& mapsDirectory) {
+		return std::filesystem::recursive_directory_iterator{mapsDirectory} | std::views::transform([&mapsDirectory](const auto& entry) {
+			return MapEntry{std::filesystem::relative(entry.path(), mapsDirectory).u8string(), entry.path()};
+		}) | std::views::filter([](const auto& entry) { return entry.path.extension() == ".map"; }) |
+			   std::ranges::to<std::vector>();
+	}
+
+
 	std::filesystem::path m_mapsBaseDirectory;
-	std::vector<std::filesystem::path> m_maps = getMaps(m_model, m_mapsBaseDirectory);
+	std::vector<MapEntry> m_maps = getMaps(m_model, m_mapsBaseDirectory);
 };
