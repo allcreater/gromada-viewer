de13d0e32e33cc77ce16a22e0ca7b7b4294b59e2
Anton Semenov <allcreater@gmail.com>
2026-09-25 23:04:11 +0300

    [feature] displaying information about armies

--- src/view_models/map_properties.cpp
+++ src/view_models/map_properties.cpp
@@ -16,14 +16,48 @@ public:
         : m_model{model} {}
 
     void updateUI() {
-        const auto activeLevel = m_model.component<ActiveLevel>();
-        auto & header = activeLevel.ensure<MapHeaderRawData>();
-
+        const auto& activeLevel = m_model.component<ActiveLevel>();
+        const auto& header = activeLevel.ensure<MapHeaderRawData>();
 
         MyImUtils::Text("Size: {} x {}", header.width, header.height);
         MyImUtils::Text("Observer pos: {}, {}", header.observerX, header.observerY);
         MyImUtils::Text("Scale: {}/{}", header.scaleX, header.scaleY);
         MyImUtils::Text("Start timer: {}", header.startTimer);
+
+        if (ImGui::CollapsingHeader("Armies")) {
+            const auto& armies = activeLevel.ensure<Armies>();
+
+            constexpr ImGuiTableFlags flags = ImGuiTableFlags_SizingStretchSame | ImGuiTableFlags_BordersInnerV;
+            if (ImGui::BeginTable("Armies", 2, flags)) {
+                ImGui::TableSetupColumn("Army 0");
+                ImGui::TableSetupColumn("Army 1");
+                ImGui::TableHeadersRow();
+                ImGui::TableNextRow();
+
+                for (const auto& army : armies) {
+                    ImGui::TableNextColumn();
+
+                    MyImUtils::Text("a/b/c: {}/{}/{}", army.a, army.b, army.c);
+                    MyImUtils::Text("Flagman id: {}", army.flagman_id);
+
+                    ImGui::Spacing();
+                    ImGui::TextUnformatted("Squads:");
+                    for (const auto& [squadIndex, squad] : std::views::enumerate(army.squads)) {
+                        const auto members = squad
+                            | std::views::transform([](std::uint32_t id) { return std::to_string(id); })
+                            | std::views::join_with(std::string_view{", "})
+                            | std::ranges::to<std::string>();
+
+                        MyImUtils::Text("  {}: [{}]", squadIndex, members);
+                    }
+                }
+
+                ImGui::EndTable();
+            }
+        }
+
+        ImGui::NewLine();
+        ImGui::TextWrapped( "Note: this is read-only visualisation of raw data from loaded map.\nThe map size and squads will be written automatically on map save.\nSome params are just fixed, some seems not important.\nPurpose of some parameters is unknown yet");
     }
 
 private:
